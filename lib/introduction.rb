214ca2f91f8af33937def7a3b0584f43fd892ffa
Dean Burgess <dinocastiglione@ymail.com>
2019-07-21 20:21:46 +0000

    Done.

---
+++ lib/introduction.rb
@@ -0,0 +1,18 @@
+def introduction(name)
+  puts "Hi, my name is #{name}."
+end 
+introduction("name")
+
+
+
+def introduction_with_language(name, language)
+  puts "Hi, my name is #{name} and I am learning to program in #{language}."
+end
+introduction_with_language("name", "language")
+
+
+
+def introduction_with_language_optional(name, language= "Ruby")
+ puts "Hi, my name is #{name} and I am learning to program in #{language}."
+end
+introduction_with_language_optional("name", "Ruby")
